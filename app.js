684f95d9c91138b563f68bea3b42c0d0ca8caa68
yashh-dev <yashwanthkrishna5027@gmail.com>
2022-12-14 20:30:40 +0530

    mongoose setup & other dependencies

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require("express");
-
 const app = express();
+const mongoose = require("mongoose");
 
-app.use('/',(req,res)=>{
-    res.send("Test")
+mongoose.set({strictQuery:true})
+mongoose.connect("mongodb://localhost:27017/blogsite"
+).then(()=>{
+    console.log("Succesfull Connected to Database");
+}).catch((e)=>{
+    console.log("Problem with mongoose");
+    console.log(e);
 })
+
+app.get('/',(req,res)=>{
+    res.send("Home")
+})
+
+
 
 const port = process.env.PORT || 3000;
 app.listen(port,()=>{
