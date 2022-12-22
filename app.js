5ff1ff34ad30bf2dc0dada7f56eae2498bf0e261
yashh-dev <yashwanthkrishna5027@gmail.com>
2022-12-22 17:51:39 +0530

    connectRetry-for-mongodb

--- app.js
+++ app.js
@@ -2,14 +2,21 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
-mongoose.set({strictQuery:true})
-mongoose.connect("mongodb://localhost:27017/blogsite"
-).then(()=>{
-    console.log("Succesfull Connected to Database");
-}).catch((e)=>{
-    console.log("Problem with mongoose");
-    console.log(e);
-})
+
+const connectRetry = function () {
+	mongoose.set({ strictQuery: true });
+	mongoose
+		.connect("mongodb://localhost:27017/blogsite")
+		.then(() => {
+			console.log("Succesfull Connected to Database");
+		})
+		.catch((e) => {
+			console.log("Problem with mongoose");
+			console.log(e);
+			setTimeout(connectRetry, 1000);
+		});
+};
+connectRetry();
 
 app.get('/',(req,res)=>{
     res.send("Home")
