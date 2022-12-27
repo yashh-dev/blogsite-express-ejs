8f8b196fc7bb251de0dc737f5d7bc135e07bf74e
Yashwanth Krishna <76145601+yashh-dev@users.noreply.github.com>
2022-12-27 14:37:24 +0000

    feat:signup router

--- routes/auth.js
+++ routes/auth.js
@@ -2,3 +2,12 @@ const User = require("../models/user");
 const express = require("express");
 
 const router = express.Router();
+
+router.route('/signup')
+.post((req,res)=>{
+    const {username , email , password } = req.body;
+    console.log(req.body);
+    res.send(req.body);
+})
+
+module.exports = router;
