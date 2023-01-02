f6096154404f2146799d76735fcc69c7e343dd77
yashh-dev <yashwanthkrishna5027@gmail.com>
2023-01-02 10:33:46 +0530

    feat:signup error handling

--- routes/auth.js
+++ routes/auth.js
@@ -6,8 +6,33 @@ const router = express.Router();
 router.route("/signup").post(async (req, res) => {
 	const { username, email, password } = req.body;
 	console.log(req.body);
-	const user = await User.create(req.body);
-	res.send(req.body);
+	try {
+		const user = await User.create(req.body);
+		user.save();
+		res.status(200).json({
+			status: "success",
+		});
+	} catch (e) {
+		console.log(e);
+		res.status(400).json({
+			status: "failed",
+			data: {
+				message: e.code,
+			},
+		});
+		return;
+	}
+});
+
+router.route("/login").post(async (req, res) => {
+	const { username, email, password } = req.body;
+	console.log(req.body);
+	const user = User.findOne({ username });
+	if (user) {
+		res.status(200).json({
+			status: "success",
+		});
+	}
 });
 
 module.exports = router;
