a50d9083d6e274fff7a15b4c4d469d8e4d8ecfb5
yashh-dev <yashwanthkrishna5027@gmail.com>
2023-01-07 09:10:36 +0530

    postSchema

--- models/post.js
+++ models/post.js
@@ -1,6 +1,23 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const postSchema = new Schema({});
+const postSchema = new Schema(
+	{
+		title: String,
+		author: {
+			type: Schema.Types.ObjectId,
+			ref: "User",
+		},
+		blocks: [
+			{
+				image: {
+					type: String,
+				},
+				content: String,
+			},
+		],
+	},
+	{ timestamps: true }
+);
 
 module.exports = mongoose.model("Post", postSchema);
