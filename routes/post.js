e7edb8140a5dc4a5d46aa26dc17b433f4269d3d3
yashh-dev <yashwanthkrishna5027@gmail.com>
2023-01-08 19:34:07 +0530

    feat:post update and delete routes v1

--- routes/post.js
+++ routes/post.js
@@ -6,12 +6,43 @@ const router = express.Router();
 router
 	.route("/")
 	.post(async (req, res) => {
-		const { title, author, blocks } = req.body;
-		const Post = Post.create(title, author, blocks);
+		const post = Post.create(req.body);
 		res.send(req.body).status(200);
 	})
 	.get(async (req, res) => {
 		res.send("Test");
+	});
+
+router
+	.route("/:id")
+	.patch(async (req, res) => {
+		console.log("hit");
+		try {
+			const post = await Post.findByIdAndUpdate(req.params.id, req.body);
+			post.save();
+			res.status(200).json({
+				status: "Updated",
+				data: req.body,
+			});
+		} catch {
+			res.status(400).json({
+				status: "Failed",
+			});
+		}
+	})
+	.delete(async (req, res) => {
+		console.log("hit");
+		try {
+			const post = await Post.findByIdAndDelete(req.params.id);
+			res.status(200).json({
+				status: "Deleted",
+				data: post,
+			});
+		} catch {
+			res.status(400).json({
+				status: "Failed to Delete",
+			});
+		}
 	});
 
 module.exports = router;
